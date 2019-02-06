e627f7bd00c022d27a8bb7f50e3b54d4d20268ca
Shiket <shiket95@gmail.com>
2019-02-06 14:47:31 +0100

    fixed props issue

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import League from './Components/League';
 import Gallery from './Components/Gallery';
 import FavTeams from './Components/FavTeams';
 import Axios from 'axios';
-import SerieA from './Components/img/serieA.png'
-import LaLiga from './Components/img/laLiga.png'
+// import SerieA from './Components/img/serieA.png'
+// import LaLiga from './Components/img/laLiga.png'
 import PremierLeague from './Components/img/premierLeague.png'
-import Eredivisie from './Components/img/erediv.png'
-import Bundesliga from './Components/img/bundesLiga.png'
-import Ligue1 from './Components/img/ligue1.png'
+// import Eredivisie from './Components/img/erediv.png'
+// import Bundesliga from './Components/img/bundesLiga.png'
+// import Ligue1 from './Components/img/ligue1.png'
 
 
 class App extends Component {
@@ -31,35 +31,33 @@ class App extends Component {
     }
 
     Axios.get(config.url + 'PL/standings?standingType=TOTAL', config)
-      .then(resp => {
+      .then(res => {
         let tmp = [];
+
         tmp.push({
-          name: resp.data.competition.name,
-          country: resp.data.competition.area.name,
-          ico: {PremierLeague},
-          firstPlace: resp.data.standings[0].table[0].team.name,
-          secondPlace: resp.data.standings[0].table[1].team.name
+          name: res.data.competition.name,
+          country: res.data.competition.area.name,
+          logo: { PremierLeague },
+          firstPlace: res.data.standings[0].table[0].team.name,
+          secondPlace: res.data.standings[0].table[1].team.name
         })
-        this.setState(state => {
-         const leagues = state.leagues.concat(tmp);
-          return {
-            leagues
-          };
-        });
+
+        this.setState ({
+          leagues: [...tmp]
+        })
       })
-alert(this.state.leagues)
   }
 
-
   render() {
+    if (this.state.leagues.length === 0) return <div>Loading</div>
     return (
       <Router className="App">
         <div className="row">
           <div className="col-12"><Header /></div>
           <div className="col-2"><FavTeams /></div>
           <div className="col-10">
-            <Route exact path="/" component={League} />
-            <Route path="/Components/Gallery" component={Gallery}/>
+            <Route exact path="/" render={() => <League teamObj={{ team: this.state.leagues }} />} />
+            <Route path="/Components/Gallery" component={Gallery} />
           </div>
         </div>
       </Router>
--- src/Components/League.js
+++ src/Components/League.js
@@ -2,33 +2,28 @@ import React, { Component } from 'react';
 import './components.css';
 import Gtrophy from './img/g-tro.png'
 import Strophy from './img/s-tro.png'
-import SerieA from './img/serieA.png'
-import LaLiga from './img/laLiga.png'
-import PremierLeague from './img/premierLeague.png'
-import Eredivisie from './img/erediv.png'
-import Bundesliga from './img/bundesLiga.png'
-import Ligue1 from './img/ligue1.png'
 
 const League = (props) => {
+    console.log(props)
         return (
             <div>
                 <div className="row mt-5 d-flex align-items-center">
                     <div className="offset-1"></div>
                     <div className="col-1">
-                        <img src={PremierLeague} alt="LeagueLogo" width="90" height="90"></img></div>
+                        <img src={props.teamObj.team[0].logo.PremierLeague} alt="LeagueLogo" width="90" height="90"></img></div>
                     <div className="col-9">
                         <div className="leagueBox">
                             <div className="row">
                                 <div className="container d-flex pt-3">
-                                    <div className="col-3"><span className="pl-3">{props.name}</span></div>
-                                    <div className="col-3"><span className="pl-3">ENGLAND</span></div>
+                                    <div className="col-3"><span className="pl-3">{props.teamObj.team[0].name}</span></div>
+                                    <div className="col-3"><span className="pl-3">{props.teamObj.team[0].country.toUpperCase()}</span></div>
                                     <div className="topTeams col-3">
                                         <img className="mr-3" src={Gtrophy} alt="goldTrophy" width="27" height="27"></img>
-                                        <span>FC Liverpool</span>
+                                        <span>{props.teamObj.team[0].firstPlace}</span>
                                     </div>
                                     <div className="topTeams col-3">
                                         <img className="mr-3" src={Strophy} alt="silverTrophy " width="27" height="27"></img>
-                                        <span>Manchester City</span>
+                                        <span>{props.teamObj.team[0].secondPlace}</span>
                                     </div>
                                 </div>
                             </div>

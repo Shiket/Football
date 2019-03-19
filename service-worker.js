aae5fddf3fab726224f6e537ab1063b9dd5b3a24
Shiket <shiket95@gmail.com>
2019-03-19 19:21:28 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Football/precache-manifest.a556d8c484ac3d08cb71885268b91744.js"
+  "/football/precache-manifest.7700d4e01c16bd2fd032c22a87040531.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Football/index.html", {
+workbox.routing.registerNavigationRoute("/football/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
